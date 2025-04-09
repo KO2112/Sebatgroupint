bb999e786e6b610044859c58d9a747b8ca1c8b1f
kemal <kemal182182@gmail.com>
2025-04-09 16:18:22 +0100

    inital commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-
-import Navbar from "@/components/Navbar";
 import AboutUs from "@/components/AboutUs";
 import NewsReports from "@/components/NewsReports";
 import Footer from "@/components/Footer";
